6ad324ca8296e814867c73837f5769315407358c
Gorka Aldasoro <finidicoder@gmail.com>
2021-12-03 09:54:56 +0100

    new!: crear los metodos post y get para trabajar con alexa

---
+++ AlexaSkillsAnasinf/Utils/AlexaActions.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace AlexaSkillsAnasinf.Utils
+{
+    public class AlexaActions
+    {
+        public static SkillResponse MakeSkillResponse(string responseText)
+        {
+            return Alexa.NET.ResponseBuilder.Tell(responseText);
+        }
+        public static SkillResponse AskSkillResponse(string responseText)
+        {
+            var reprompt = new Reprompt("No he encontrado nada");
+            return Alexa.NET.ResponseBuilder.Ask(responseText, reprompt);
+        }
+    }
+}
---
+++ AlexaSkillsAnasinf/Utils/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using Alexa.NET.Response;
+global using Alexa.NET.Request;
+global using Alexa.NET.Request.Type;
+global using Microsoft.AspNetCore.Mvc;
