1432ffab3f8d348ff47f1de59e58e5361b73e8f1
Gorka Aldasoro <finidicoder@gmail.com>
2021-12-06 14:03:50 +0100

    new! crear el proyecto de datos de fichajes

---
+++ AlexaSkillsAnasinf.Fichajes.Data/SqlConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace AlexaSkillsAnasinf.Fichajes.Data
+{
+    public class SqlConfiguration
+    {
+        public string ConnectionString { get; }
+        public SqlConfiguration(string connectionString) => ConnectionString = connectionString;
+    }
+}
